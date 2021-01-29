ba11d2bb7c842389a5a4058a532aa6274262460b
Wie.Gh <dnlrhksgus0511@eloicube.com>
2021-01-29 15:44:15 +0900

    test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Wietility
 {
@@ -18,7 +19,7 @@ namespace Wietility
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            
             await builder.Build().RunAsync();
         }
     }
